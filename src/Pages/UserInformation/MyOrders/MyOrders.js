6b652279233708d13ef5521131ec324cfd41175a
Akahad <aksahad234567@gmail.com>
2022-11-26 12:34:48 +0600

    8th commit

--- src/Pages/UserInformation/MyOrders/MyOrders.js
+++ src/Pages/UserInformation/MyOrders/MyOrders.js
@@ -1,9 +1,73 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../AuthProvider/AuthProvider';
+import Myorder from '../Myoder/Myorder';
 
 const MyOrders = () => {
+    const{user}=useContext(AuthContext)
+    const [oders,setoders]=useState([])
+    useEffect(()=>{
+        fetch(`http://localhost:5000/booking?email=${user?.email}`)
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            setoders(data)
+            
+        })
+
+    },[user?.email])
     return (
         <div>
-            <h1>This si </h1>
+            <div className="overflow-x-auto mt-5">
+  <table className="table w-full">
+    
+    <thead>
+      <tr>
+        <th></th>
+       
+        <th>Name</th>
+        <th>Price</th>
+        
+        <th>Location</th>
+        <th>Pyment</th>
+      </tr>
+    </thead>
+    <tbody>
+    {
+        oders.map((order,i)=><tr>
+        <th>{i+1}</th>
+        
+  
+        <td>{order.ProductName}</td>
+        <td>{order.ProductPrice}</td>
+        
+        <td>{order.loction}</td>
+        
+      
+      <td>{
+      order.ProductPrice && !order.paid && <Link to={`/deshborad/payment/${order._id}`}>
+      <button className='btn btn-primary btn-xs'>Pay</button>
+      
+      </Link> 
+      }
+      ,{
+        order.Price && order.paid && <button className='text-green-400'>Paid</button>
+      }
+      </td>
+      </tr>)
+      }
+        
+      
+      
+      
+      
+    </tbody>
+  </table>
+</div>
             
         </div>
     );
---
+++ src/Pages/UserInformation/Myoder/Myorder.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Myorder = ({order}) => {
+    const {name,img,ProductNamc}=order
+    return (
+        <div>
+            
+
+                <th>1</th>
+                <td>Cy Ganderton</td>
+                <td>Quality Control Specialist</td>
+                <td>Blue</td>
+            
+        </div>
+    );
+};
+
+export default Myorder;
